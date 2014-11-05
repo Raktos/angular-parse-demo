12b0d3ba4da60b310255a3c0adfa0e0ab290a82e
Jason Ho <raktos@uw.edu>
2014-11-05 11:03:39 -0800

    github requests complete

--- js/github-app.js
+++ js/github-app.js
@@ -8,11 +8,16 @@
 
 
 angular.module('GitHubApp', [])
-    .controller('GitHubController', function($scope) {
-        $scope.userName = 'drstearns';
+    .controller('GitHubController', function($scope, $http) {
+        $scope.userName = '';
         $scope.getRepos = function() {
-
-            //add code here
-
+            $http.get('https://api.github.com/users/' + $scope.userName + '/repos')
+                .success(function(data) {
+                    $scope.repos = data;
+                    $scope.errorMessage = null;
+                })
+                .error(function(err) {
+                    $scope.errorMessage = err.message || 'User does not exist.'
+                });
         };
     });
